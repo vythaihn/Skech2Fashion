39566018f3df926bdb14fd9e3d2a4b48c395033a
vythaihn <vythai@stanford.edu>
2020-11-09 22:16:00 -0800

    new

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function pickColor() {
 }
 
 function pickImage() {
-  var pic = getRandomInt(1, 13)
+  var pic = getRandomInt(1, 1759)
   pic_name = pic.toString();
   var file_name = pic_name.concat(".png")
 
